ab69d905095fc3c4751e60f77f13fd88b4ffffd7
vladhaidukkk <the.vladhaiduk@gmail.com>
2022-07-03 19:04:22 +0300

    feat: setup project for heroku deploy

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const chalk = require("chalk");
-const config = require("config");
-const initDatabase = require("./startUp/initDatabase");
-const routes = require("./routes");
-const cors = require("cors");
-const path = require("path");
+const express = require('express');
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+const config = require('config');
+const initDatabase = require('./startUp/initDatabase');
+const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
 
-const PORT = config.get("port") ?? 8080;
+const PORT = process.env.PORT ?? config.get('port') ?? 8080;
 
 const app = express();
 
@@ -15,25 +15,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use("/api", routes);
+app.use('/api', routes);
 
-if (process.env.NODE_ENV === "production") {
-  const root = path.join(__dirname, "client");
-  const indexFile = path.join(root, "index.html");
+if (process.env.NODE_ENV === 'production') {
+  const root = path.join(
+    __dirname,
+    process.env.HEROKU ? '../client/build' : 'client'
+  );
+  const indexFile = path.join(root, 'index.html');
 
-  app.use("/", express.static(root));
-  app.get("*", (req, res) => {
+  app.use('/', express.static(root));
+  app.get('*', (req, res) => {
     res.sendFile(indexFile);
   });
 }
 
 const start = async () => {
   try {
-    mongoose.connection.once("open", async () => {
+    mongoose.connection.once('open', async () => {
       await initDatabase();
     });
-    await mongoose.connect(config.get("mongodbUri"));
-    console.log(chalk.green("Server was connected to MongoDB"));
+    await mongoose.connect(config.get('mongodbUri'));
+    console.log(chalk.green('Server was connected to MongoDB'));
     app.listen(PORT, () => {
       console.log(chalk.green(`Server is running on PORT: ${PORT}`));
     });
